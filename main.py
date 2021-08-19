b3d7af80ea0d9c8b417841bea258d34bad13f690
zqtang10 <73584901+zqtang10@users.noreply.github.com>
2021-08-19 11:08:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ def cmd():
         elif get_command == 'goto windows cmd.exe':
             print("TkOS:Open app\"cmd.exe\"is succcessfully.\nIf you want to exit the cmd.exe,please insert the\"exit\"command to exit the cmd.exe")
             os.system('cmd')
+        elif get_command == 'run textedit':
+            print("open textedit")
+            text_edit()
         else:
             print("sorry, your command is not a vaild TkOS command.")
         input_command.delete(0,tkinter.END)
@@ -80,6 +83,10 @@ def update_log():
     update = tkinter.Text(log)
     update.pack()
     update_text = '版本更新日志（按版本编号从大到小排列）\n\
+0.0.114514.3:\n\
+完善部分UI。\n\
+完善文本编辑。\n\
+新增程序打开器。\n\
 0.0.114514.2：\n\
 更新了更新日志的查看。\n\
 新增文本编辑。\n\
@@ -92,7 +99,30 @@ def update_log():
     update.insert(tkinter.END, update_text)
 
 def text_edit():
-    windows = tkinter.Text()
+    def open():
+        file_open = filedialog.askopenfile(title = '打开文件', parent = windows, filetypes = [("文本文档", '.txt', \
+            '所有文档', '.*')])
+        with open(file_open, 'r') as file:
+            text.insert(tkinter.END, file)
+            file.close()
+    def save():
+        file_text = text.get(1.0, tkinter.END)
+        file_name = filedialog.asksaveasfile(title = '保存/另存为文件...', parent = windows, filetypes = \
+            [("文本文档", '.txt'), ("所有文件", '.*')])
+        if  not file_name:
+            return
+        with open(file_name, 'w') as file:
+            file.write(file_text)
+            file.close()
+
+    def new():
+        if text.get(1.0, tkinter.END) == '':
+            text.delete(1.0, tkinter.END)
+        else:
+            want = messagebox.askquestion(message="是否要保存当前的文件？")
+            if want == True:
+                save()
+    windows = tkinter.Toplevel()
     windows.title('TkOS文本编辑器')
     text = tkinter.Text(windows)
     text.pack()
@@ -100,11 +130,30 @@ def text_edit():
     file_menu = tkinter.Menu(text_menu, tearoff = 0)
     help_menu = tkinter.Menu(text_menu, tearoff = 0)
     text_menu.add_cascade(label = '文件(F)', menu = file_menu)
-    file_menu.add_command(label = '新建')
-    file_menu.add_command(label = '保存'
+    file_menu.add_command(label = '新建', command = new)
+    file_menu.add_command(label = '保存', command = save)
     file_menu.add_command(label = '另存为')
+    file_menu.add_command(label = '打开')
+    file_menu.add_separator()
     help_menu.add_command(label = '关于')
     windows.config(menu = text_menu)
+
+def open_app():
+    def ok():
+        app_will_open = var.get()
+        if app_will_open == "文本编辑":
+            text_edit()
+        elif app_will_open == "终端":
+            cmd()
+
+    app = tkinter.Toplevel()
+    app.title("打开应用程序")
+    tkinter.Label(app, text = '请选择应用程序：').pack()
+    var = tkinter.StringVar()
+    app_select = ttk.Combobox(app, textvariable=var)
+    app_select['value'] = ('文本编辑', '终端')
+    app_select.pack()
+    ttk.Button(app ,text = '确定', command = ok).pack()
 
 def about():
     about_windows = tkinter.Toplevel()
@@ -122,6 +171,7 @@ system.add_command(label = '关机系统', command = shutdown)
 system.add_command(label = "关机（注意这个真的会关机）", command = shutdown_windows)
 system.add_separator()
 system.add_command(label = '终端', command = cmd)
+system.add_command(label = '打开应用程序', command = open_app)
 system.add_separator()
 system.add_command(label = '更新日志', command = update_log)
 system.add_command(label = '关于',command = about)
